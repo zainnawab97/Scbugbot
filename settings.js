02fe04c578a7636f55cbcfe34b82a93f00baa72b
zainnawab97 <115409891+zainnawab97@users.noreply.github.com>
2023-11-05 20:03:48 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.botname = '𝐙𝐀𝐈𝐍✯𝐍𝐚𝐰𝐚𝐛𝐙𝐚𝐚𝐃𝐚�
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = '🦄𝐙𝐀𝐈𝐍✯𝐍𝐚𝐰𝐚𝐛𝐙𝐚𝐚𝐃𝐚𝐚✯𝘽𝙊𝙏\n\nContact: +923074060856'
+global.author = '𝐙𝐀𝐈𝐍✯𝐍𝐚𝐰𝐚𝐛𝐙𝐚𝐚𝐃𝐚𝐚✯𝘽𝙊𝙏\n\nContact: +923074060856'
 
 //console view/theme
 global.themeemoji = '🪀'
